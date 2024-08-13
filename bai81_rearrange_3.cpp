38b0a46e0fddf6a244e8ba6f0a93fa476f5466cd
thuytt <thuytt0295@gmail.com>
2024-08-13 22:52:11 +0700

    add: sliding window algorithm

--- bai81_rearrange_3.cpp
+++ bai81_rearrange_3.cpp
@@ -1,7 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main (){
+/*
+5 3
+2 1 5 6 3
+
+7 5
+2 7 9 5 8 7 4
+*/
+
+
+void solve (){
+	int n, k;
+	cin >> n >> k;
+	int a[n];
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	
+	
+	// count element that less than or equal k
+	int total_good_numbers = 0, bad_numbers = 0;
+	
+	for(int i = 0; i < n; i++){
+		if(a[i] <= k){
+			total_good_numbers++;
+		}
+	}
+	
+	for(int i = 0; i < total_good_numbers; i++){
+		if(a[i] > k){
+			bad_numbers++;
+		}
+	}
+	
+	int swap = bad_numbers;
+	for(int l = 0, r = total_good_numbers; l < n, r < n; l++, r++){
+		// if the r pointer a[r] > k => increment bad_numbers
+		if(a[r] > k) bad_numbers++;
+		// remove bad_numbers if a[l] = 0 because want to keep equal to the size window (total_good_numbers)
+		if(a[l] > k) bad_numbers--;	
+		swap = min(swap, bad_numbers);
+	}
+	
+	cout << swap << endl;	
+}
+
+int main (){	
+	solve();
 	
 	return 0;
 }
